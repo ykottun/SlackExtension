e1cd3ad3e34b8080b5bbd1f8bbc9cc04ac560a25
ykottun <ykottun@gmail.com>
2025-02-19 16:18:41 +0900

    Listen to messages

--- slackextension/app.py
+++ slackextension/app.py
@@ -2,9 +2,17 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+# ボットトークンを渡してアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-# アプリを起動します
+# 'こんにちは' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://tools.slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("こんにちは")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"こんにちは、<@{message['user']}> さん！")
+
 if __name__ == "__main__":
+    # アプリを起動して、ソケットモードで Slack に接続します
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
